4336e6aa446f04df12ab5e6ea79fe610024f1c4a
vimal004 <2004.vimal@gmail.com>
2024-06-21 13:39:32 +0530

    c2

--- src/main/java/com/example/employee/demo/Controller/EmployeeController.java
+++ src/main/java/com/example/employee/demo/Controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +29,13 @@ public class EmployeeController {
     @GetMapping("/{id}")
     public Employee getEmployeeById(@PathVariable int id) {
         return employeeService.getEmployeeById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Employee updateEmployee(@PathVariable int id, @RequestBody Employee employee) {
+        Employee updatedEmployee = employeeService.getEmployeeById(id);
+        updatedEmployee.setName(employee.getName());
+        return employeeService.saveEmployee(updatedEmployee);
     }
 
     @PostMapping
